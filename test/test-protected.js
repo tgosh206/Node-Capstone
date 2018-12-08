32a770d0854af62475834465d44035a2f56d5b60
Tanner Goshert <tgosh206@hotmail.com>
2018-12-08 14:22:16 -0800

    added test-protected.js

---
+++ test/test-protected.js
@@ -0,0 +1,144 @@
+'use strict';
+global.DATABASE_URL = 'mongodb://user-1:user-1@ds053788.mlab.com:53788/node-capstone';
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const jwt = require('jsonwebtoken');
+
+const {app, runServer, closeServer} = require('../server');
+const {User} = require('../users');
+const {JWT_SECRET} = require('../config');
+
+const expect = chai.expect;
+
+// Lets us make HTTP requests
+chai.use(chaiHttp);
+
+describe('Protected endpoint', function() {
+  const username = 'exampleUser';
+  const password = 'examplePass';
+
+  before(function() {
+    return runServer();
+  });
+
+  after(function() {
+    return closeServer();
+  });
+
+  beforeEach(function() {
+    return User.hashPassword(password).then(password =>
+      User.create({
+        username,
+        password
+      })
+    );
+  });
+
+  afterEach(function() {
+    return User.remove({});
+  });
+
+  describe('/api/protected', function() {
+    it('Should reject requests with no credentials', function() {
+      return chai
+        .request(app)
+        .get('/api/protected')
+        .then(() =>
+          expect.fail(null, null, 'Request should not succeed')
+        )
+        .catch(err => {
+          if (err instanceof chai.AssertionError) {
+            throw err;
+          }
+
+          const res = err.response;
+          expect(res).to.have.status(401);
+        });
+    });
+
+    it('Should reject requests with an invalid token', function() {
+      const token = jwt.sign(
+        {
+          username
+        },
+        'wrongSecret',
+        {
+          algorithm: 'HS256',
+          expiresIn: '7d'
+        }
+      );
+
+      return chai
+        .request(app)
+        .get('/api/protected')
+        .set('Authorization', `Bearer ${token}`)
+        .then(() =>
+          expect.fail(null, null, 'Request should not succeed')
+        )
+        .catch(err => {
+          if (err instanceof chai.AssertionError) {
+            throw err;
+          }
+
+          const res = err.response;
+          expect(res).to.have.status(401);
+        });
+    });
+    it('Should reject requests with an expired token', function() {
+      const token = jwt.sign(
+        {
+          user: {
+            username
+          },
+          exp: Math.floor(Date.now() / 1000) - 10 // Expired ten seconds ago
+        },
+        JWT_SECRET,
+        {
+          algorithm: 'HS256',
+          subject: username
+        }
+      );
+
+      return chai
+        .request(app)
+        .get('/api/protected')
+        .set('authorization', `Bearer ${token}`)
+        .then(() =>
+          expect.fail(null, null, 'Request should not succeed')
+        )
+        .catch(err => {
+          if (err instanceof chai.AssertionError) {
+            throw err;
+          }
+
+          const res = err.response;
+          expect(res).to.have.status(401);
+        });
+    });
+    it('Should send protected data', function() {
+      const token = jwt.sign(
+        {
+          user: {
+            username
+          }
+        },
+        JWT_SECRET,
+        {
+          algorithm: 'HS256',
+          subject: username,
+          expiresIn: '7d'
+        }
+      );
+
+      return chai
+        .request(app)
+        .get('/api/protected')
+        .set('authorization', `Bearer ${token}`)
+        .then(res => {
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an('object');
+          expect(res.body.data).to.equal('rosebud');
+        });
+    });
+  });
+});
